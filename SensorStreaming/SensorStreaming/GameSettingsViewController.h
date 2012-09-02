9101db6ae2f47f5b02e9e8e6ff207f32ebf003a6
Qays Poonawala <qayshp@gmail.com>
2012-09-02 00:52:19 -0700

    Start page done, working on settings page.

---
+++ SensorStreaming/SensorStreaming/GameSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameSettingsViewController.h
+//  SensorStreaming
+//
+//  Created by Qays Poonawala on 2012-09-01.
+//  Copyright (c) 2012 Orbotix, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameSettingsViewController : UIPageViewController
+
+@end
---
+++ SensorStreaming/SensorStreaming/HomeScreenViewController.m
@@ -0,0 +1,44 @@
+//
+//  HomeScreenViewController.m
+//  SensorStreaming
+//
+//  Created by Qays Poonawala on 2012-09-01.
+//  Copyright (c) 2012 Orbotix, Inc. All rights reserved.
+//
+
+#import "HomeScreenViewController.h"
+
+@interface HomeScreenViewController ()
+
+@end
+
+@implementation HomeScreenViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
